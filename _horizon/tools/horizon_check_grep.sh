bbd9823bdacec982deb264036ef938b01214d9c4
KOCMOHABT <25280520+KOCMODECAHTHUK@users.noreply.github.com>
2026-09-03 03:05:49 +0500

    add: Mod Linters

---
+++ _horizon/tools/horizon_check_grep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#ANSI Escape Codes for colors to increase contrast of errors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+NC="\033[0m" # No Color
+
+echo -e "${BLUE}Re-running grep checks, but looking in _horizon/...${NC}"
+
+# Run the linters again, but modular horizon code.
+sed 's/code\/\*\*\/\*\*.dm/_horizon\/features\/\*\*\/\*\*.dm/g' <tools/ci/check_grep.sh | bash
+
+echo -e "${BLUE}Re-running grep checks, but looking in _horizon/...${NC}"
